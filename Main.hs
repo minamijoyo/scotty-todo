63993ccc5b6c1e4069683fc506860499e80b76f5
minamijoyo <minamijoyo@gmail.com>
2014-12-20 16:12:56 +0900

    Add index view

--- Main.hs
+++ Main.hs
@@ -1,14 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Web.Scotty
-import Text.Blaze.Html5
-import Text.Blaze.Html5.Attributes
-import qualified Web.Scotty as S
-import qualified Text.Blaze.Html5 as H
-import qualified Text.Blaze.Html5.Attributes as A
 import Text.Blaze.Html.Renderer.Text
+
+import qualified Todo.Views.Index
+
+blaze = html . renderHtml
 
 main :: IO ()
 main = scotty 3000 $ do
   get "/" $ do
-    S.html . renderHtml $ do
-      h1 "My todo list"
+    blaze Todo.Views.Index.render
