0d7219098dc549652d671aa0d4088547d1d6e17d
minamijoyo <minamijoyo@gmail.com>
2014-12-20 16:19:53 +0900

    Add static html

--- Main.hs
+++ Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Web.Scotty
 import Text.Blaze.Html.Renderer.Text
+import Network.Wai.Middleware.Static
 
 import qualified Todo.Views.Index
 
@@ -10,3 +11,7 @@ main :: IO ()
 main = scotty 3000 $ do
   get "/" $ do
     blaze Todo.Views.Index.render
+
+  get "/404" $ file "404.html"
+
+  middleware $ staticPolicy (noDots >-> addBase "static")
