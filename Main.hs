aebd2b0dd5cfdac9c5b7f33b9c69d593277a85a9
minamijoyo <minamijoyo@gmail.com>
2014-12-20 15:41:02 +0900

    Check User-Agent header

--- Main.hs
+++ Main.hs
@@ -1,6 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Web.Scotty
 
+main :: IO ()
 main = scotty 3000 $ do
-  get "/" $ do
-    html "Hello World!"
+  get "/agent" $ do
+    agent <- header "User-Agent"
+    case agent of
+      Just ua -> text ua
+      Nothing -> text "no User-Agent header"
